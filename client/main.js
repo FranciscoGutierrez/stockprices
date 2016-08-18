8d095065022b30641a8e595021cb53364a914123
FranciscoGutierrez <fsalvador23@gmail.com>
2016-08-18 12:58:40 +0200

    Updated gitignore

--- client/main.js
+++ client/main.js
@@ -3,18 +3,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.slider.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.slider.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.slider.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
